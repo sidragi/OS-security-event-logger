6500c75807c8019cb7a5dd1a97949f0cd4c2ce80
Siddhivinayak Lahiri <sidlahiri@Siddhivinayaks-MacBook-Air.local>
2025-03-19 15:50:30 +0530

    displayed text on main window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,9 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->labelTitle->setText("OS Project CSE316"); //here labletitle is the object which is the title and the set text is an function in the header which changes the title to whatevre ive written there
+    ui->labelTitle->setAlignment(Qt::AlignCenter);// this one is similar to the above one it juts makes the aligmnent of the text in the center
+
 }
 
 MainWindow::~MainWindow()
