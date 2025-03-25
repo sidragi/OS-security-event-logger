c3e39742714fd82804f76189217dcf17bb15b595
Sid Lahiri <sidlahiri04@gmail.com>
2025-03-26 04:41:10 +0530

    this version has multiple generla errors which occur in the sys logs and a toggleable button is added

--- build/Desktop_arm_darwin_generic_mach_o_64bit-Debug/ui_mainwindow.h
+++ build/Desktop_arm_darwin_generic_mach_o_64bit-Debug/ui_mainwindow.h
@@ -15,7 +15,7 @@
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
-#include <QtWidgets/QPlainTextEdit>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QTabWidget>
 #include <QtWidgets/QTableWidget>
@@ -33,11 +33,29 @@ public:
     QWidget *tab;
     QVBoxLayout *verticalLayout;
     QTableWidget *realTimeSecurity;
+    QPushButton *pushButton;
     QTabWidget *tabWidget_2;
     QWidget *tab_3;
     QHBoxLayout *horizontalLayout_3;
-    QPlainTextEdit *realTimeSecurityAuthFailure;
+    QTableWidget *realTimeSecurityAuthFail;
     QWidget *tab_4;
+    QVBoxLayout *verticalLayout_2;
+    QTableWidget *realTimeSecuritySudoFail;
+    QWidget *tab_5;
+    QVBoxLayout *verticalLayout_3;
+    QTableWidget *realTimeSecurityCodeSignIssue;
+    QWidget *tab_6;
+    QVBoxLayout *verticalLayout_4;
+    QTableWidget *realTimeSecurityGatekeeper;
+    QWidget *tab_8;
+    QVBoxLayout *verticalLayout_5;
+    QTableWidget *realTimeSecurityKeychain;
+    QWidget *tab_7;
+    QVBoxLayout *verticalLayout_7;
+    QTableWidget *realTimeSecurityFileAccess;
+    QWidget *tab_9;
+    QVBoxLayout *verticalLayout_6;
+    QTableWidget *realTimeSecuritySIPViolation;
     QWidget *tab_2;
     QMenuBar *menubar;
     QStatusBar *statusbar;
@@ -46,14 +64,14 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName("MainWindow");
-        MainWindow->resize(800, 600);
+        MainWindow->resize(919, 600);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName("centralwidget");
         horizontalLayout_2 = new QHBoxLayout(centralwidget);
         horizontalLayout_2->setObjectName("horizontalLayout_2");
         tabWidget = new QTabWidget(centralwidget);
         tabWidget->setObjectName("tabWidget");
-        tabWidget->setTabPosition(QTabWidget::TabPosition::North);
+        tabWidget->setTabPosition(QTabWidget::TabPosition::West);
         tabWidget->setTabShape(QTabWidget::TabShape::Rounded);
         tabWidget->setIconSize(QSize(32, 32));
         tabWidget->setElideMode(Qt::TextElideMode::ElideRight);
@@ -70,21 +88,84 @@ public:
 
         verticalLayout->addWidget(realTimeSecurity);
 
+        pushButton = new QPushButton(tab);
+        pushButton->setObjectName("pushButton");
+
+        verticalLayout->addWidget(pushButton);
+
         tabWidget_2 = new QTabWidget(tab);
         tabWidget_2->setObjectName("tabWidget_2");
+        tabWidget_2->setTabBarAutoHide(false);
         tab_3 = new QWidget();
         tab_3->setObjectName("tab_3");
         horizontalLayout_3 = new QHBoxLayout(tab_3);
         horizontalLayout_3->setObjectName("horizontalLayout_3");
-        realTimeSecurityAuthFailure = new QPlainTextEdit(tab_3);
-        realTimeSecurityAuthFailure->setObjectName("realTimeSecurityAuthFailure");
+        realTimeSecurityAuthFail = new QTableWidget(tab_3);
+        realTimeSecurityAuthFail->setObjectName("realTimeSecurityAuthFail");
 
-        horizontalLayout_3->addWidget(realTimeSecurityAuthFailure);
+        horizontalLayout_3->addWidget(realTimeSecurityAuthFail);
 
         tabWidget_2->addTab(tab_3, QString());
         tab_4 = new QWidget();
         tab_4->setObjectName("tab_4");
+        verticalLayout_2 = new QVBoxLayout(tab_4);
+        verticalLayout_2->setObjectName("verticalLayout_2");
+        realTimeSecuritySudoFail = new QTableWidget(tab_4);
+        realTimeSecuritySudoFail->setObjectName("realTimeSecuritySudoFail");
+
+        verticalLayout_2->addWidget(realTimeSecuritySudoFail);
+
         tabWidget_2->addTab(tab_4, QString());
+        tab_5 = new QWidget();
+        tab_5->setObjectName("tab_5");
+        verticalLayout_3 = new QVBoxLayout(tab_5);
+        verticalLayout_3->setObjectName("verticalLayout_3");
+        realTimeSecurityCodeSignIssue = new QTableWidget(tab_5);
+        realTimeSecurityCodeSignIssue->setObjectName("realTimeSecurityCodeSignIssue");
+
+        verticalLayout_3->addWidget(realTimeSecurityCodeSignIssue);
+
+        tabWidget_2->addTab(tab_5, QString());
+        tab_6 = new QWidget();
+        tab_6->setObjectName("tab_6");
+        verticalLayout_4 = new QVBoxLayout(tab_6);
+        verticalLayout_4->setObjectName("verticalLayout_4");
+        realTimeSecurityGatekeeper = new QTableWidget(tab_6);
+        realTimeSecurityGatekeeper->setObjectName("realTimeSecurityGatekeeper");
+
+        verticalLayout_4->addWidget(realTimeSecurityGatekeeper);
+
+        tabWidget_2->addTab(tab_6, QString());
+        tab_8 = new QWidget();
+        tab_8->setObjectName("tab_8");
+        verticalLayout_5 = new QVBoxLayout(tab_8);
+        verticalLayout_5->setObjectName("verticalLayout_5");
+        realTimeSecurityKeychain = new QTableWidget(tab_8);
+        realTimeSecurityKeychain->setObjectName("realTimeSecurityKeychain");
+
+        verticalLayout_5->addWidget(realTimeSecurityKeychain);
+
+        tabWidget_2->addTab(tab_8, QString());
+        tab_7 = new QWidget();
+        tab_7->setObjectName("tab_7");
+        verticalLayout_7 = new QVBoxLayout(tab_7);
+        verticalLayout_7->setObjectName("verticalLayout_7");
+        realTimeSecurityFileAccess = new QTableWidget(tab_7);
+        realTimeSecurityFileAccess->setObjectName("realTimeSecurityFileAccess");
+
+        verticalLayout_7->addWidget(realTimeSecurityFileAccess);
+
+        tabWidget_2->addTab(tab_7, QString());
+        tab_9 = new QWidget();
+        tab_9->setObjectName("tab_9");
+        verticalLayout_6 = new QVBoxLayout(tab_9);
+        verticalLayout_6->setObjectName("verticalLayout_6");
+        realTimeSecuritySIPViolation = new QTableWidget(tab_9);
+        realTimeSecuritySIPViolation->setObjectName("realTimeSecuritySIPViolation");
+
+        verticalLayout_6->addWidget(realTimeSecuritySIPViolation);
+
+        tabWidget_2->addTab(tab_9, QString());
 
         verticalLayout->addWidget(tabWidget_2);
 
@@ -98,7 +179,7 @@ public:
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName("menubar");
-        menubar->setGeometry(QRect(0, 0, 800, 24));
+        menubar->setGeometry(QRect(0, 0, 919, 24));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName("statusbar");
@@ -106,8 +187,8 @@ public:
 
         retranslateUi(MainWindow);
 
-        tabWidget->setCurrentIndex(1);
-        tabWidget_2->setCurrentIndex(0);
+        tabWidget->setCurrentIndex(0);
+        tabWidget_2->setCurrentIndex(4);
 
 
         QMetaObject::connectSlotsByName(MainWindow);
@@ -116,8 +197,14 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "General Errors", nullptr));
         tabWidget_2->setTabText(tabWidget_2->indexOf(tab_3), QCoreApplication::translate("MainWindow", "Authentication Failure", nullptr));
-        tabWidget_2->setTabText(tabWidget_2->indexOf(tab_4), QCoreApplication::translate("MainWindow", "Tab 2", nullptr));
+        tabWidget_2->setTabText(tabWidget_2->indexOf(tab_4), QCoreApplication::translate("MainWindow", "Sudo Failures", nullptr));
+        tabWidget_2->setTabText(tabWidget_2->indexOf(tab_5), QCoreApplication::translate("MainWindow", "Code Signing Issues", nullptr));
+        tabWidget_2->setTabText(tabWidget_2->indexOf(tab_6), QCoreApplication::translate("MainWindow", "Gatekeeper", nullptr));
+        tabWidget_2->setTabText(tabWidget_2->indexOf(tab_8), QCoreApplication::translate("MainWindow", "Keychain", nullptr));
+        tabWidget_2->setTabText(tabWidget_2->indexOf(tab_7), QCoreApplication::translate("MainWindow", "File Access Restrictions", nullptr));
+        tabWidget_2->setTabText(tabWidget_2->indexOf(tab_9), QCoreApplication::translate("MainWindow", "SIP Violations", nullptr));
         tabWidget->setTabText(tabWidget->indexOf(tab), QCoreApplication::translate("MainWindow", "Tab1", nullptr));
         tabWidget->setTabText(tabWidget->indexOf(tab_2), QCoreApplication::translate("MainWindow", "Tab 2", nullptr));
     } // retranslateUi
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,12 +9,43 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+    ui->tabWidget_2->setVisible(false);
     QProcess *process=new QProcess(this); // the this keyword is used to make it a child of the mainwindow class
 
     ui->realTimeSecurity->setColumnCount(5);//we are gonna have 5 columns in the table so its beeen set t 5 the column names are mentionedbwlow
     QStringList column = {"Timestamp", "Type", "Activity", "PID", "Message"};
     ui->realTimeSecurity->setHorizontalHeaderLabels(column);//sethoriheadr lable basically puts the q string on the colun names
     ui->realTimeSecurity->horizontalHeader()->setStretchLastSection(true);// it makes sure that the last column is streach to fit the fill winod w
+    //it bacially is a copy of the above and it will come for the others as well
+    ui->realTimeSecurityAuthFail->setColumnCount(5);
+    ui->realTimeSecurityAuthFail->setHorizontalHeaderLabels(column);
+    ui->realTimeSecurityAuthFail->horizontalHeader()->setStretchLastSection(true);
+
+    ui->realTimeSecuritySudoFail->setColumnCount(5);
+    ui->realTimeSecuritySudoFail->setHorizontalHeaderLabels(column);
+    ui->realTimeSecuritySudoFail->horizontalHeader()->setStretchLastSection(true);
+
+    ui->realTimeSecurityCodeSignIssue->setColumnCount(5);
+    ui->realTimeSecurityCodeSignIssue->setHorizontalHeaderLabels(column);
+    ui->realTimeSecurityCodeSignIssue->horizontalHeader()->setStretchLastSection(true);
+
+    ui->realTimeSecurityGatekeeper->setColumnCount(5);
+    ui->realTimeSecurityGatekeeper->setHorizontalHeaderLabels(column);
+    ui->realTimeSecurityGatekeeper->horizontalHeader()->setStretchLastSection(true);
+
+    ui->realTimeSecuritySIPViolation->setColumnCount(5);
+    ui->realTimeSecuritySIPViolation->setHorizontalHeaderLabels(column);
+    ui->realTimeSecuritySIPViolation->horizontalHeader()->setStretchLastSection(true);
+
+    ui->realTimeSecurityKeychain->setColumnCount(5);
+    ui->realTimeSecurityKeychain->setHorizontalHeaderLabels(column);
+    ui->realTimeSecurityKeychain->horizontalHeader()->setStretchLastSection(true);
+
+    ui->realTimeSecurityFileAccess->setColumnCount(5);
+    ui->realTimeSecurityFileAccess->setHorizontalHeaderLabels(column);
+    ui->realTimeSecurityFileAccess->horizontalHeader()->setStretchLastSection(true);
+
+
 
     connect(process,&QProcess::readyReadStandardOutput,this,[this,process](){ // connect is a signal-slot thing which connects a signal to a slot. Here, the signal is the process (i.e., the log file) and the slot where the signal is to be sent is the QPlainTextEdit, which is a function of this. The [this, process] is a function that allows the object here (the plain text edit) to get the text here
         QString out=process->readAllStandardOutput().trimmed();// now this puts the output in a string and its then given to the function which slices the string and gives the output
@@ -32,6 +63,12 @@ MainWindow::MainWindow(QWidget *parent)
     // log stream --predicate 'subsystem == "com.apple.security"'
     // the log stream thing is used to filter the logs only with com.apple.security
     // macOS has a unified log system, so we have to filter it accordingly. The com.apple.security is a subsystem.
+
+
+    connect(ui->pushButton, &QPushButton::clicked, this, [this]() {// this is used to make the additional menuse toggleable and, it uses an inline function
+        ui->tabWidget_2->setVisible(!ui->tabWidget_2->isVisible());
+    });
+
 
 }
 
@@ -64,10 +101,70 @@ void MainWindow::splitNshow(const QString &logEntry,const QString &defaultType)/
 
     ui->realTimeSecurity->scrollToBottom(); //auto scrools to the botton when new stuff arrives
 
+    if (message.contains("authentication", Qt::CaseInsensitive)) { // this makes sure that the failed auth messege individually also goes to the the failed auth table
+        int rowAuth = ui->realTimeSecurityAuthFail->rowCount();
+        ui->realTimeSecurityAuthFail->insertRow(rowAuth);
+
+        ui->realTimeSecurityAuthFail->setItem(rowAuth,0,new QTableWidgetItem(timestamp));
+        ui->realTimeSecurityAuthFail->setItem(rowAuth,1,new QTableWidgetItem(type.isEmpty() ? defaultType : type));
+        ui->realTimeSecurityAuthFail->setItem(rowAuth,2,new QTableWidgetItem(activity));
+        ui->realTimeSecurityAuthFail->setItem(rowAuth,3,new QTableWidgetItem(pid));
+        ui->realTimeSecurityAuthFail->setItem(rowAuth,4,new QTableWidgetItem(message));
+        ui->realTimeSecurityAuthFail->scrollToBottom();
+    }
+    if (message.contains("sudo", Qt::CaseInsensitive)) {
+        int rowSudo = ui->realTimeSecuritySudoFail->rowCount();
+        ui->realTimeSecuritySudoFail->insertRow(rowSudo);
+        ui->realTimeSecuritySudoFail->setItem(rowSudo, 0, new QTableWidgetItem(timestamp));
+        ui->realTimeSecuritySudoFail->setItem(rowSudo, 1, new QTableWidgetItem(type));
+        ui->realTimeSecuritySudoFail->setItem(rowSudo, 2, new QTableWidgetItem(activity));
+        ui->realTimeSecuritySudoFail->setItem(rowSudo, 3, new QTableWidgetItem(pid));
+        ui->realTimeSecuritySudoFail->setItem(rowSudo, 4, new QTableWidgetItem(message));
+        ui->realTimeSecuritySudoFail->scrollToBottom();
+    }
+
+    if (message.contains("codesign", Qt::CaseInsensitive)) {
+        int rowCodeSign = ui->realTimeSecurityCodeSignIssue->rowCount();
+        ui->realTimeSecurityCodeSignIssue->insertRow(rowCodeSign);
+        ui->realTimeSecurityCodeSignIssue->setItem(rowCodeSign, 0, new QTableWidgetItem(timestamp));
+        ui->realTimeSecurityCodeSignIssue->setItem(rowCodeSign, 1, new QTableWidgetItem(type));
+        ui->realTimeSecurityCodeSignIssue->setItem(rowCodeSign, 2, new QTableWidgetItem(activity));
+        ui->realTimeSecurityCodeSignIssue->setItem(rowCodeSign, 3, new QTableWidgetItem(pid));
+        ui->realTimeSecurityCodeSignIssue->setItem(rowCodeSign, 4, new QTableWidgetItem(message));
+        ui->realTimeSecurityCodeSignIssue->scrollToBottom();
+    }
+
+    if (message.contains("gatekeeper", Qt::CaseInsensitive)) {
+        int rowGatekeeper = ui->realTimeSecurityGatekeeper->rowCount();
+        ui->realTimeSecurityGatekeeper->insertRow(rowGatekeeper);
+        ui->realTimeSecurityGatekeeper->setItem(rowGatekeeper, 0, new QTableWidgetItem(timestamp));
+        ui->realTimeSecurityGatekeeper->setItem(rowGatekeeper, 1, new QTableWidgetItem(type));
+        ui->realTimeSecurityGatekeeper->setItem(rowGatekeeper, 2, new QTableWidgetItem(activity));
+        ui->realTimeSecurityGatekeeper->setItem(rowGatekeeper, 3, new QTableWidgetItem(pid));
+        ui->realTimeSecurityGatekeeper->setItem(rowGatekeeper, 4, new QTableWidgetItem(message));
+        ui->realTimeSecurityGatekeeper->scrollToBottom();
+    }
+
+    if (message.contains("keychain", Qt::CaseInsensitive)) {
+        int rowKeychain = ui->realTimeSecurityKeychain->rowCount();
+        ui->realTimeSecurityKeychain->insertRow(rowKeychain);
+        ui->realTimeSecurityKeychain->setItem(rowKeychain, 0, new QTableWidgetItem(timestamp));
+        ui->realTimeSecurityKeychain->setItem(rowKeychain, 1, new QTableWidgetItem(type));
+        ui->realTimeSecurityKeychain->setItem(rowKeychain, 2, new QTableWidgetItem(activity));
+        ui->realTimeSecurityKeychain->setItem(rowKeychain, 3, new QTableWidgetItem(pid));
+        ui->realTimeSecurityKeychain->setItem(rowKeychain, 4, new QTableWidgetItem(message));
+        ui->realTimeSecurityKeychain->scrollToBottom();
+    }
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+
 }
 
